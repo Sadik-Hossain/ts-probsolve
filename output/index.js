efe1ee34f75d8e67394e31f7c395612fa01d6736
sadik <po480373@gmai.com>
2022-10-02 12:16:01 +0600

    no.2 solved

--- output/index.js
+++ output/index.js
@@ -1,7 +1,14 @@
 "use strict";
 // ### problem no:1 https://www.codechef.com/submit/BURGERS
-function burger(a, b) {
+function burger(patties, bun) {
     //1 burger needs 1bun , 1pattie
-    const z = Math.min(a, b);
-    return z;
+    const burger = Math.min(patties, bun);
+    return burger;
 }
+// ### problem no:2 https://www.codechef.com/submit/DETSCORE
+function score(totalPoints = 10, Correct) {
+    const pointsPerQuestions = totalPoints / 10;
+    const earnedPoints = Correct * pointsPerQuestions;
+    return earnedPoints;
+}
+console.log(score(70, 0));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 // ### problem no:1 https://www.codechef.com/submit/BURGERS
-function burger(a:number,b:number){
+function burger(patties:number,bun:number){
 
 
     //1 burger needs 1bun , 1pattie
-  const z = Math.min(a,b)
-        return z;
+  const burger = Math.min(patties,bun)
+        return burger;
     }
+
+    // ### problem no:2 https://www.codechef.com/submit/DETSCORE
+    function score(totalPoints:number=10 ,Correct:number){
+const pointsPerQuestions:number=totalPoints/10
+const earnedPoints:number = Correct*pointsPerQuestions
+return earnedPoints
+}
+console.log(score(70,0))
