1cdef4b5b87a18670350404aa7c0f6ea126f1c0c
sadik <po480373@gmai.com>
2022-10-02 14:10:11 +0600

    no.3 4 6 solved

--- output/index.js
+++ output/index.js
@@ -11,4 +11,36 @@ function score(totalPoints = 10, Correct) {
     const earnedPoints = Correct * pointsPerQuestions;
     return earnedPoints;
 }
-console.log(score(70, 0));
+// console.log(score(70,0))
+// ### problem no:3 https://www.codechef.com/submit/MY1STCONTEST
+function contest(users, unsubmittedUser, unsolvedUser) {
+    const submittedUser = users - unsubmittedUser;
+    const solvedUser = submittedUser - unsolvedUser;
+    return [submittedUser, solvedUser];
+}
+//    console.log(contest(10,3,2))
+//    console.log(contest(20,1,2))
+//    console.log(contest(1000,300,700))
+// ### problem no:4 https://www.codechef.com/submit/PRIZEPOOL
+function prizeMoney(topTenPrize, afterTopTenPrize) {
+    const totalPrizeMoney = 10 * topTenPrize + 90 * afterTopTenPrize;
+    return totalPrizeMoney;
+}
+// console.log(prizeMoney(1000,100));
+// console.log(prizeMoney(1000,1000));
+// console.log(prizeMoney(80,1));
+// console.log(prizeMoney(400,30));
+// ### problem no:5 https://www.codechef.com/submit/PRACTICEPERF
+// function practicePerf (arguments) {
+//     // body
+// }
+// ### problem no:6 https://www.codechef.com/submit/NUM239
+function prettyNum(start, end) {
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+        arr.push(i.toString().charAt(i.toString().length - 1));
+    }
+    return (arr.filter(e => e == "2" || e == "3" || e == "9").length);
+}
+// console.log(prettyNum(1,10)) 
+// console.log(prettyNum(11,33)) 
--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,42 @@ const pointsPerQuestions:number=totalPoints/10
 const earnedPoints:number = Correct*pointsPerQuestions
 return earnedPoints
 }
-console.log(score(70,0))
+// console.log(score(70,0))
+
+   // ### problem no:3 https://www.codechef.com/submit/MY1STCONTEST
+   function contest (users:number,unsubmittedUser:number,unsolvedUser:number) {
+    const submittedUser = users-unsubmittedUser;
+    const solvedUser  = submittedUser-unsolvedUser;  
+return [submittedUser,solvedUser]
+   }
+//    console.log(contest(10,3,2))
+//    console.log(contest(20,1,2))
+//    console.log(contest(1000,300,700))
+
+// ### problem no:4 https://www.codechef.com/submit/PRIZEPOOL
+function prizeMoney (topTenPrize:number,afterTopTenPrize:number ) {
+const totalPrizeMoney = 10*topTenPrize + 90*afterTopTenPrize
+return totalPrizeMoney
+}
+// console.log(prizeMoney(1000,100));
+// console.log(prizeMoney(1000,1000));
+// console.log(prizeMoney(80,1));
+// console.log(prizeMoney(400,30));
+
+
+// ### problem no:5 https://www.codechef.com/submit/PRACTICEPERF
+// function practicePerf (arguments) {
+//     // body
+// }
+
+// ### problem no:6 https://www.codechef.com/submit/NUM239
+function prettyNum (start:number,end:number) {
+    let arr:string[]=[]
+for(let i=start;i<=end;i++){
+  arr.push(i.toString().charAt(i.toString().length-1))
+}
+return(arr.filter(e=>e=="2"||e=="3"||e=="9").length)
+
+}
+// console.log(prettyNum(1,10)) 
+// console.log(prettyNum(11,33)) 
