5344b366c219a25aa02e96a1736f288207e114d0
sadik <po480373@gmai.com>
2022-10-02 11:53:29 +0600

    init

---
+++ output/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// ### problem no:1 https://www.codechef.com/submit/BURGERS
+function burger(a, b) {
+    //1 burger needs 1bun , 1pattie
+    const z = Math.min(a, b);
+    return z;
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// ### problem no:1 https://www.codechef.com/submit/BURGERS
+function burger(a:number,b:number){
+
+
+    //1 burger needs 1bun , 1pattie
+  const z = Math.min(a,b)
+        return z;
+    }
